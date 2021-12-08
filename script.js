67ad5fe31063f0bdb3fd9cd0e0b561da94c7d672
@heissonly <yerlikaya.ugur@gmail.com>
2021-12-08 21:17:12 +0100

    Latest Changs

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+	principal = document.getElementById('principal').value;
+    rate= document.getElementById('rate').value;
+    years=document.getElementById('years').value;
+    result=document.getElementById('result');
+
+    var interest = principal * years * rate / 100.0;
+    var year = new Date().getFullYear()+parseInt(years);
+	
+	if(principal < 0 || principal == null || principal == 0){
+        alert("Enter a positive number");
+        document.getElementById("principal").focus();
+        return;
+    }
+	
+	  result.innerHTML= `If you deposit <mark>${principal}</mark>,<br/>at an interest rate of <mark>${rate} %</mark>.<br/>You will receive an amount of <mark>${interest}</mark>,<br/>in the year <mark>${year}</mark>`;
 }
-        
+
+
+  function updateRate() {
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
+}
+
+	
